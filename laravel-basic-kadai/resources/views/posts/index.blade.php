19bdbe64909f7796ac31b829d3fa72d4b23cc049
hachioji159 <hachioji159>
2024-07-23 11:50:55 +0900

    posts migration

--- laravel-basic-kadai/resources/views/posts/index.blade.php
+++ laravel-basic-kadai/resources/views/posts/index.blade.php
@@ -9,6 +9,24 @@
 
 <body>
   <h1>投稿一覧</h1>
+  <table border="1">
+    <tr>
+      <th>ID</th>
+      <th>タイトル</th>
+      <th>本文</th>
+      <th>投稿日時</th>
+      <th>更新日時</th>
+    </tr>
+    @foreach($posts as $post)
+    <tr>
+      <td>{{ $post->id }}</td>
+      <td>{{ $post->title }}</td>
+      <td>{{ $post->contents }}</td>
+      <td>{{ $post->created_at }}</td>
+      <td>{{ $post->updated_at }}</td>
+    </tr>
+    @endforeach
+  </table>
 </body>
 
 </html>
